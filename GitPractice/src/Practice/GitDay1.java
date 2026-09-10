2d9c60d58c6df34e494d2aecd8965214fc72b36f
asikahamed-0 <asikahamed.web@gmail.com>
2026-09-10 10:30:00 +0530

    Day 1 -Git practice

---
+++ GitPractice/src/Practice/GitDay1.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class GitDay1 {
+
+
+    public static void main(String[] args) {
+
+       
+        System.out.println("My Git and GitHub Journey Started!");
+
+    }
+}
